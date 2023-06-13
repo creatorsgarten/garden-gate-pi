884cadcf1ac3de2f5e8097e69e57d53b9352de63
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-13 22:08:55 +0700

    ugh perms

--- ota-updater/daemon.sh
+++ ota-updater/daemon.sh
@@ -4,9 +4,9 @@ while true
 do
   if [ -e ota-updater/data/trigger ]
   then
-    sudo rm ota-updater/data/trigger
+    rm ota-updater/data/trigger
     echo "Update requested at $(date)" >> ota-updater/data/log
-    ./scripts/update.sh >> ota-updater/data/log 2>&1
+    (sudo -u garten ./scripts/update.sh) >> ota-updater/data/log 2>&1
   fi
   sleep 30
 done
